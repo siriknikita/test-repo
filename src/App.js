f5b11a771ce04428c69ea4069a55dd83204a9247
siriknikita <mykyta.siryk@lnu.edu.ua>
2024-08-20 12:40:43 +0300

    something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { NumericFormat } from "react-number-format";
+import Input from "@material-ui/core/Input";
+import { makeStyles } from "@material-ui/core/styles";
+
 
 function App() {
+  const inputRef = useRef(null);
+  const [quantity, setQuantity] = useState(1);
+
+  const handleInputOnChange = (e) => {
+    setQuantity(e.target.value);
+  }
+
+  const handleInputOnBlur = () => {
+    if (quantity === "") {
+      setQuantity(1);
+    }
+  }
+
+  const handleOnKeyUp = (e) => {
+    if (e.key === "Enter") {
+      inputRef.current.blur();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NumericFormat
+      key="regular-cart-step"
+      allowNegative={false}
+      customInput={Input}
+      type="number"
+      inputProps={{
+        step: 1,
+        inputMode: "numeric",
+      }}
+      inputRef={inputRef}
+      disableUnderline
+      value={quantity}
+      valueIsNumericString
+      onChange={handleInputOnChange}
+      onBlur={handleInputOnBlur}
+      onKeyUp={handleOnKeyUp}
+    />
   );
 }
 
